ed109178cc27217402d72e2bf7e95bb1851c785b
Vova Vasilev <vova.vasilev@yahoo.com>
2018-09-13 00:10:44 +0500

    some little changes

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 
-const { exec } = require("child_process");
-const command = `json-server --watch db.json --port ${port}`;
-
-exec(command);
-
 app.use(express.static(__dirname + "/dist"));
 
 app.get("*", (req, res) => {
