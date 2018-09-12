da58348549c9251fa3ee651d86bbc26f7f400cf8
Vova Vasilev <vova.vasilev@yahoo.com>
2018-09-12 22:24:39 +0500

    changed json-server configuration

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 
-// the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname + "/dist"));
 
-// send the user to index html page inspite of the url
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/dist", "index.html"));
 });
@@ -17,6 +15,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+
 server.use(middlewares);
 server.use(router);
 server.listen(port, function() {
